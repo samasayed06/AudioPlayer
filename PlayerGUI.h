9812e89b8645493b6480a4cab5ff635681ca7cfb
nada <20240630@stud.fci-cu.edu.eg>
2025-10-22 23:03:40 +0300

    I deleted a comment

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,41 +1,4 @@
-﻿/*
-  ==============================================================================
-
-    PlayerGUI.h
-    Created: 21 Oct 2025 4:34:07pm
-    Author:  USER
-
-  ==============================================================================
-*/
-/*#pragma once
-#include <JuceHeader.h>
-#include "PlayerAudio.h"
-
-class PlayerGUI : public juce::Component
-{
-public:
-    PlayerGUI(PlayerAudio& player);
-    ~PlayerGUI() override = default;
-
-    void paint(juce::Graphics&) override;
-    void resized() override;
-
-private:
-    PlayerAudio& audioPlayer;
-
-    juce::TextButton loadButton{ "Load" };
-    juce::TextButton playButton{ ">" };
-    juce::TextButton pauseButton{ "||" };
-    juce::TextButton restartButton{ "Restart" };
-    juce::TextButton startButton{ "|<" };
-    juce::TextButton endButton{ ">|" };
-
-    std::unique_ptr<juce::FileChooser> fileChooser;
-
-    void setupButtons();
-};*/
-
-
+﻿
 #pragma once
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
